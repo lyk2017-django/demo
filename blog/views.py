754d5f8794d5ae12ed2e0998daa35939d5c46180
Umut Karcı <cediddi@gmail.com>
2017-07-07 21:29:36 +0300

    View'lara giriş

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def anasayfa(request):
-    return render(request, "blog/anasayfa.html")
+    return render(request, "blog/anasayfa.html", {"isim": request.POST.get("isim")})
