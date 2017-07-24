22f4204ec3d54d21a145b0ac14fb3aeaaddf1ba5
Umut Karcı <cediddi@gmail.com>
2017-07-24 18:51:41 +0300

    detay sayfası

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     url("^$", anasayfa, name="home"),
     url("^list$", list_entry, name="listele"),
+    url("^show/(?P<entry_id>\d+)/$", detail_entry, name="goster")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from django.urls import reverse
 
 from blog.models import Entry
 
@@ -13,5 +15,26 @@ def anasayfa(request):
 def list_entry(request):
     return render(request, "blog/listele.html",
                   {"entries": Entry.objects.filter(is_reported=False)})
+
+
+def detail_entry(request, entry_id):
+    entry = get_object_or_404(Entry, id=entry_id, is_reported=False)
+    updates = []
+    entry.reads += 1
+    if "like" in request.POST:
+        entry.likes += 1
+        updates.append("likes")
+    else:
+        entry.likes += 1
+        updates.append("reads")
+    if "report" in request.POST:
+        entry.is_reported = True
+        updates.append("is_reported")
+    entry.save(update_fields=updates)
+
+    if "is_reported" in updates:
+        return HttpResponseRedirect(redirect_to=reverse("listele"))
+    return render(request, "blog/goster.html",
+                  {"entry": entry})
 
 
